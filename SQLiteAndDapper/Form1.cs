ec393ff9e0e5e7cce23d5c091360df73c3461996
chumpon-asaneerat <chumponsenate@gmail.com>
2020-07-13 14:01:10 +0700

    Update 2020-07-13 #8 - Update SQLite and Dapper sample prroject #8.   - Test add 20000 records from 2 processes.

--- SQLiteAndDapper/Form1.cs
+++ SQLiteAndDapper/Form1.cs
@@ -13,6 +13,8 @@ using System.Windows.Forms;
 
 #endregion
 
+using Dapper;
+
 namespace SQLiteAndDapper
 {
     public partial class Form1 : Form
@@ -24,6 +26,22 @@ namespace SQLiteAndDapper
 
         private SQLiteConnectionStringBuilder builder = new SQLiteConnectionStringBuilder();
         private SQLiteConnection conn;
+
+        private void RefreshGrid()
+        {
+            dataGridView1.DataSource = null;
+            List<MyRecord> items;
+            if (null != conn)
+            {
+                items = conn.Query<MyRecord>("Select * From MyRecord").ToList();
+            }
+            else
+            {
+                items = new List<MyRecord>();
+            }
+
+            dataGridView1.DataSource = items;
+        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -56,18 +74,36 @@ namespace SQLiteAndDapper
 
         private void button1_Click(object sender, EventArgs e)
         {
-            for (int i = 0; i < 200; i++)
+            if (null == conn) return;
+            //List<MyRecord> items = new List<MyRecord>();
+            for (int i = 0; i < 20000; i++)
             {
-
+                MyRecord inst = new MyRecord();
+                inst.Data = "Data-" + i.ToString("D5");
+                inst.DOB = DateTime.Now;
+                conn.Execute("INSERT INTO MyRecord(Data, DOB) VALUES(@Data, @DOB)", inst);
             }
+            
             RefreshGrid();
         }
 
-        private void RefreshGrid()
+        private void button2_Click(object sender, EventArgs e)
         {
-            dataGridView1.DataSource = null;
-
-            dataGridView1.DataSource = null;
+            RefreshGrid();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (null == conn) return;
+            conn.Execute("DELETE FROM MyRecord");
+            RefreshGrid();
+        }
+    }
+
+    public class MyRecord
+    {
+        public int Id { get; set; }
+        public string Data { get; set; }
+        public DateTime DOB { get; set; }
     }
 }
